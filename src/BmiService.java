5bc435f808a9fe4141e47618d53b3953b0b8ba03
Vladimir Arger <8745@rambler.ru>
2023-05-16 16:11:25 +0400

    initial commit

---
+++ src/BmiService.java
@@ -0,0 +1,13 @@
+public class BmiService {
+    public double calculate (double weight, double height){
+        double boodyMassIndex = (weight / (height*height));
+        return boodyMassIndex;
+    };
+
+    /* todo:
+    формула индекса массы тела
+    i = m / (h*h)
+    приведение дробного числа к целому
+    int b = (int) a;
+     */
+}
